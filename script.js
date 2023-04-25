d9d367aca93d884257b8b35d10fb9ef3d55273fd
Nikolay <areyfclm@gmail.com>
2023-04-25 20:17:50 +0300

    mobile menu new view

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
 const burger = document.querySelector('.burger-menu')
 const headerMenu = document.querySelector('.header-menu')
-const callToClient = document.querySelector('.call-back')
 
-const handleBurger = () => {
+burger.addEventListener('click', function() {
     headerMenu.classList.toggle('active-menu')
-}
-
-burger.addEventListener('click', handleBurger)
-
-callToClient.addEventListener('click', function() {
-        (function(w,d,u){
-                var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);
-                var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
-        })(window,document,'https://cdn-ru.bitrix24.ru/b24058702/crm/site_button/loader_24_jabgbp.js');
 })
+
