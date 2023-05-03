fe9a97bc6bf1454496a359b40b7fc0f0fb1e9600
Nikolay <areyfclm@gmail.com>
2023-05-03 14:54:26 +0300

    b24 fixes

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const burger = document.querySelector('.burger-menu')
 const headerMenu = document.querySelector('.header-menu')
+const input = document.querySelector('.input')
+const title = document.querySelector('.title')
 
 burger.addEventListener('click', function() {
     headerMenu.classList.toggle('active-menu')
 })
 
+input.addEventListener('change', () => {
+    let userName = input.value
+    title.innerHTML = `Привет, ${userName}!`
+    input.classList.add('disabled')
+    input.value = ''
+})
