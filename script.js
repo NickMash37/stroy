c6e1c314ea3f192d04213466862f0be788274fad
Nikolay <areyfclm@gmail.com>
2023-05-05 13:19:34 +0300

    b24 v.1.1, callback button added

--- script.js
+++
@@ -1,6 +0,0 @@
-const burger = document.querySelector('.burger-menu')
-const headerMenu = document.querySelector('.header-menu')
-
-burger.addEventListener('click', function() {
-    headerMenu.classList.toggle('active-menu')
-})
